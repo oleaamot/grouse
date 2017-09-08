8dbcc3a07c4c1b2916c1a61436fe142b7becbfe8
Thomas Sødring <tsodring@gmail.com>
2017-09-08 13:15:37 +0200

    Initial commit og spring-app

---
+++ kravspec/src/main/java/no/kdrs/grouse/kravspec/KravspecApplication.java
@@ -0,0 +1,12 @@
+package no.kdrs.grouse.kravspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KravspecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KravspecApplication.class, args);
+	}
+}
