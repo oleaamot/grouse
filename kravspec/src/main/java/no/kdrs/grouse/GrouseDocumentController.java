8b21b94b1486b507fd46a41bfd348d0f207ececc
Ole Aamot <oka@oka.no>
2017-10-03 19:21:16 +0200

    Add specifications

--- kravspec/src/main/java/no/kdrs/grouse/GrouseDocumentController.java
+++ kravspec/src/main/java/no/kdrs/grouse/GrouseDocumentController.java
@@ -28,7 +28,7 @@ public class GrouseDocumentController {
 	}
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public GrouseDocument getDocument(@PathVariable("id") Integer id) {
-		GrouseDocument lastDocument = this.documents.stream().skip(this.documents.size() - 1).findFirst().orElse(null);
+		GrouseDocument lastDocument = this.documents.stream().filter(document-> document.getId() == id).findFirst().orElse(null);
 		try {
 			XWPFDocument document = new XWPFDocument();
 			XWPFParagraph tmpParagraph = document.createParagraph();
@@ -106,13 +106,17 @@ public class GrouseDocumentController {
 	}
 	List<GrouseDocument> buildDocuments() {
 		List<GrouseDocument> documents = new ArrayList<>();
-
 		GrouseDocument document1 = buildDocument(1, "5.1.1", "For at et system skal kunne godkjennes etter Noark 5-standarden, må den konseptuelle modellen av arkivstrukturen og de funksjonelle muligheter den gir, kunne implementeres i det aktuelle systemets (fysiske) datastrukturer.", "noark5");
-
+		GrouseDocument document2 = buildDocument(2, "5.1.2","Arkivdokumenter som tilhører et sakarkiv skal inngå i en arkivstruktur som skal inneholde følgende arkivenheter: Arkiv, Arkivdel, Klassifikasjonssystem, Klasse, Mappe 1), Registrering2), Dokumentbeskrivelse og Dokumentobjekt. 1) Mappe er en fellesbenevning for arkivenhetene som også inkluderer spesialiseringene Saksmappe og Møtemappe. 2) Registrering er en fellesbenevning for arkivenhetene Registrering inkl. spesialiseringene Basisregistrering, Journalpost og Møteregistrering.", "noark5");
+		GrouseDocument document3 = buildDocument(3, "5.1.3", "Arkivdokumenter som ikke tilhører et sakarkiv (eks. fagsystem) kan inngå i en ”forenklet” arkivstruktur som minimum skal kunne inneholde følgende arkivenheter: Arkiv, Arkivdel, Registrering1) og Dokumentobjekt. 1) Registrering er en fellesbenevning for arkivenhetene Registrering inkl. spesialiseringene Basisregistrering, Journalpost og Møteregistrering.", "noark5");
+		GrouseDocument document4 = buildDocument(4, "5.1.4", "Noark5 skal ha tjenester/funksjoner for å lagre, gjenfinne, endre og slette data og utvalg1) av data i henhold til metadatabeskrivelsene i alle arkivenheter (se krav 5.1.2) og tilhørende klasser som er dokumentert i de konseptuelle modellene og metadatatabellene i Noark5. Merk: Funksjonelle enkeltkrav i de forskjellige kapitlene kan overstyre dette kravet. 1) Med utvalg av data menes utvalg innenfor en fra/til angivelse av obligatoriske metadata for aktuell arkivenhet.", "noark5");
+		GrouseDocument document5 = buildDocument(5, "5.1.5","En arkivenhet (se krav 5.1.2 og 5.1.3) skal kunne identifiseres entydig innenfor det arkivskapende organet. I et arkivuttrekk skal denne identifikasjonen hete systemID, og være entydig på tvers av alle uttrekk som organet produserer, dermed også på tvers av alle systemer organet benytter.  Også arkivenheter som dupliseres i et arkivuttrekk, skal identifiseres entydig, slik at identiske arkivenheter har ulik systemID.", "noark5");
 		documents.add(document1);
-
+		documents.add(document2);
+		documents.add(document3);
+		documents.add(document4);
+		documents.add(document5);
 		return documents;
-
 	}
 	GrouseDocument buildDocument(Integer id, String title, String comment, String reference) {
 		GrouseDocument document = new GrouseDocument();
