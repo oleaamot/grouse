804560ee5e8824f2e42a406c340e2f63a528732c
Ole Aamot <oka@oka.no>
2017-10-07 20:50:37 +0200

    Initial SQL map for GrouseDocument and kravspec based on Hibernate

--- kravspec/src/main/java/no/kdrs/grouse/Grouse.java
+++ kravspec/src/main/java/no/kdrs/grouse/Grouse.java
@@ -1,9 +1,26 @@
 package no.kdrs.grouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.hibernate.Session;
 @SpringBootApplication
-public class Grouse {  
+public class Grouse {
 	public static void main(String[] args) {
-		SpringApplication.run(Grouse.class, args);
-    }       
-}            
+		// SpringApplication.run(Grouse.class, args);
+		System.out.println("Grouse + Hibernate + MySQL");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		GrouseDocument doc = new GrouseDocument();
+		doc.setKravnr("5.1.1");
+		doc.setOokrav("For at et system skal kunne godkjennes etter Noark5-standarden, må den konseptuelle modellen av arkivstrukturen og de funksjonelle muligheter den gir, kunne implementeres i det aktuelle systemets (fysiske) datastrukturer.");
+		doc.setKravtype("O");
+		doc.setMerknad("TEST");
+		doc.setForklaring("TEST");
+		doc.setKonsekvens("TEST");
+		doc.setKonfnivaa("TEST");
+		doc.setRefkrav("TEST");
+		doc.setStatus("TEST");
+		doc.setAnsvar("TEJ");
+		session.save(doc);
+		session.getTransaction().commit();
+	}
+}
--- kravspec/src/main/java/no/kdrs/grouse/GrouseDocument.java
+++ kravspec/src/main/java/no/kdrs/grouse/GrouseDocument.java
@@ -2,6 +2,16 @@ package no.kdrs.grouse;
 
 public class GrouseDocument {
 	public Integer id;
+	public String kravnr;
+	public String ookrav;
+	public String kravtype;
+	public String merknad;
+	public String forklaring;
+	public String konsekvens;
+	public String konfnivaa;
+	public String refkrav;
+	public String status;
+	public String ansvar;
 	public String title;
 	public String comment;
 	public String reference;
@@ -12,22 +22,82 @@ public class GrouseDocument {
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public String getTitle() {
-		return title;
+	public void setKravnr(String kravnr) {
+		this.kravnr = kravnr;
+	}
+	public String getKravnr() {
+		return kravnr;
+	}
+	public void setOokrav(String ookrav) {
+		this.ookrav = ookrav;
+	}
+	public String getOokrav() {
+		return ookrav;
+	}
+	public void setKravtype(String kravtype) {
+		this.kravtype = kravtype;
+	}
+	public String getKravtype() {
+		return kravtype;
+	}
+	public void setMerknad(String merknad) {
+		this.merknad = merknad;
+	}
+	public String getMerknad() {
+		return merknad;
+	}
+	public void setForklaring(String forklaring) {
+		this.forklaring = forklaring;
+	}
+	public String getForklaring() {
+		return forklaring;
+	}
+	public void setKonsekvens(String konsekvens) {
+		this.konsekvens = konsekvens;
+	}
+	public String getKonsekvens() {
+		return konsekvens;
+	}
+	public void setKonfnivaa(String konfnivaa) {
+		this.konfnivaa = konfnivaa;
+	}
+	public String getKonfnivaa() {
+		return konfnivaa;
+	}
+	public void setRefkrav(String refkrav) {
+		this.refkrav = refkrav;
+	}
+	public String getRefkrav() {
+		return refkrav;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setAnsvar(String ansvar) {
+		this.ansvar = ansvar;
+	}
+	public String getAnsvar() {
+		return ansvar;
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getComment() {
-		return comment;
+	public String getTitle() {
+		return title;
 	}
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-	public String getReference() {
-		return reference;
+	public String getComment() {
+		return comment;
 	}
 	public void setReference(String reference) {
 		this.reference = reference;
+	}
+	public String getReference() {
+		return reference;
 	}
 }
