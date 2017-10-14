6fdb2a750e8c290ffc2c01a8a3737cc2263e5a19
Ole Aamot <oka@oka.no>
2017-10-14 19:11:02 +0200

    Write noark5kravspec.doc from SQL

--- kravspec/src/main/java/no/kdrs/grouse/Grouse.java
+++ kravspec/src/main/java/no/kdrs/grouse/Grouse.java
@@ -19,51 +19,52 @@ public class Grouse {
 		System.out.println("Grouse + Hibernate + MySQL");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Iterator iter = session.createQuery("from GrouseDocument").iterate();
-		while (iter.hasNext()) {
+
+		try {
+		    FileOutputStream fos = new FileOutputStream(new File("noark5kravspec.doc"));
+		    XWPFDocument document = new XWPFDocument();
+		    while (iter.hasNext()) {
 			GrouseDocument doc = (GrouseDocument) iter.next();
 			System.out.println("kravnr=" + doc.getKravnr() + "\n");
-			try {
-				XWPFDocument document = new XWPFDocument();
-				XWPFParagraph tmpParagraph = document.createParagraph();
-				XWPFRun titleRun = tmpParagraph.createRun();
-				titleRun.setText(doc.getKravnr() + "\n");
-				titleRun.setFontSize(18);
-				XWPFRun spaceRun = tmpParagraph.createRun();
-				spaceRun.setText(doc.getOokrav() + "\n");
-				spaceRun.setFontSize(18);
-				XWPFRun commentRun = tmpParagraph.createRun();
-				commentRun.setText(doc.getComment());
-				commentRun.setFontSize(18);
-				XWPFRun cRun = tmpParagraph.createRun();
-				cRun.setText("\n");
-				cRun.setFontSize(18);
-				XWPFRun referenceRun = tmpParagraph.createRun();
-				referenceRun.setText("Referanse: " + doc.getReference());
-				referenceRun.setFontSize(18);
-				// XWPFTable table = document.createTable();
-				// // create first row
-				// XWPFTableRow tableRowOne = table.getRow(0);
-				// tableRowOne.getCell(0).setText(lastDocument.getTitle());
-				// tableRowOne.addNewTableCell().setText(lastDocument.getComment());
-				// tableRowOne.addNewTableCell().setText(lastDocument.getReference());
-				// // create second row
-				// XWPFTableRow tableRowTwo = table.createRow();
-				// tableRowTwo.getCell(0).setText("col one, row two");
-				// tableRowTwo.getCell(1).setText("col two, row two");
-				// tableRowTwo.getCell(2).setText("col three, row two");
-				// // create third row
-				// XWPFTableRow tableRowThree = table.createRow();
-				// tableRowThree.getCell(0).setText("col one, row three");
-				// tableRowThree.getCell(1).setText("col two, row three");
-				// tableRowThree.getCell(2).setText("col three, row three");
-				FileOutputStream fos = new FileOutputStream(new File(doc.getKravnr() + ".doc"));
-				document.write(fos);
-				fos.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			XWPFParagraph tmpParagraph = document.createParagraph();
+			XWPFRun titleRun = tmpParagraph.createRun();
+			titleRun.setText(doc.getKravnr() + "\n");
+			titleRun.setFontSize(18);
+			XWPFRun spaceRun = tmpParagraph.createRun();
+			spaceRun.setText(doc.getOokrav() + "\n");
+			spaceRun.setFontSize(18);
+			XWPFRun commentRun = tmpParagraph.createRun();
+			commentRun.setText(doc.getComment());
+			commentRun.setFontSize(18);
+			XWPFRun cRun = tmpParagraph.createRun();
+			cRun.setText("\n");
+			cRun.setFontSize(18);
+			XWPFRun referenceRun = tmpParagraph.createRun();
+			referenceRun.setText("Referanse: " + doc.getReference());
+			referenceRun.setFontSize(18);
+			XWPFTable table = document.createTable();
+			// // create first row
+			XWPFTableRow tableRowOne = table.getRow(0);
+			tableRowOne.getCell(0).setText(doc.getOokrav());
+			tableRowOne.addNewTableCell().setText(doc.getComment());
+			tableRowOne.addNewTableCell().setText(doc.getReference());
+			// // create second row
+			// XWPFTableRow tableRowTwo = table.createRow();
+			// tableRowTwo.getCell(0).setText("col one, row two");
+			// tableRowTwo.getCell(1).setText("col two, row two");
+			// tableRowTwo.getCell(2).setText("col three, row two");
+			// // create third row
+			// XWPFTableRow tableRowThree = table.createRow();
+			// tableRowThree.getCell(0).setText("col one, row three");
+			// tableRowThree.getCell(1).setText("col two, row three");
+			// tableRowThree.getCell(2).setText("col three, row three");
+		    }
+		    document.write(fos);
+		    fos.close();
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
 		}
 		session.beginTransaction();
 		GrouseDocument doc = new GrouseDocument();
