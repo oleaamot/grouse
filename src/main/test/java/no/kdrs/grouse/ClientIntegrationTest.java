f24e01393668cbc4afeb827b29c4162253910f66
Thomas Sødring <tsodring@gmail.com>
2017-12-12 22:39:42 +0100

    Start work on testing framework
    It's important to get the tests in place early. This update has a single
    working test to help get my head around how tests actually work.

---
+++ src/main/test/java/no/kdrs/grouse/ClientIntegrationTest.java
@@ -0,0 +1,38 @@
+package no.kdrs.grouse;
+
+import no.kdrs.grouse.model.Functionality;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("demo")
+public class ClientIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void createClient() {
+
+        Functionality functionality = new Functionality();
+        functionality.setFunctionalityNumber("99");
+        functionality.setDescription("description");
+        functionality.setTitle("title");
+
+        ResponseEntity<Functionality> responseEntity =
+                restTemplate.postForEntity("/funksjon", functionality, Functionality.class);
+        Functionality client = responseEntity.getBody();
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals("description", client.getDescription());
+    }
+}
