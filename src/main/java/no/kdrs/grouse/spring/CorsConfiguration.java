8f92484cb9ee6880fb61b4d55708ef8a29cd81f0
Thomas Sødring <tsodring@gmail.com>
2018-03-28 15:55:57 +0200

    Deal with deprecated WebMvcConfigurerAdapter
    Simple change, just return WebMvcConfigurer

--- src/main/java/no/kdrs/grouse/spring/CorsConfiguration.java
+++ src/main/java/no/kdrs/grouse/spring/CorsConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class CorsConfiguration {
@@ -15,7 +14,7 @@ public class CorsConfiguration {
     public WebMvcConfigurer corsConfigurer() {
         // TODO: Should this be configred to only accept requests from localhost
         // behind apache or similar??
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
