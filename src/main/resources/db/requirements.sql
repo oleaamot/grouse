f0de96a2ea1fd2ab9b0ba88ce12d5163d0c1ac01
Thomas Sødring <tsodring@gmail.com>
2018-03-28 22:23:02 +0200

    Separate SQL insert statements into own files
    Having all of this in one file is going to eventually mean we have a
    single large messy file that is slow to load in Idea. So we separate
    the contents into their own files.
    
    Also update to properties files to reflect this change

---
+++ src/main/resources/db/requirements.sql
@@ -0,0 +1,62 @@
+
+
+
+
+INSERT INTO requirements (id, show_order, priority, functionality,
+                          requirement_text)
+VALUES (
+  1, 1,'O (i)', '1.4',
+  'Leverandøren skal gi en oversiktlig, overordnet og kortfattet beskrivelse av funksjonalitet, arkitektur, grensesnitt og virkemåte for den tilbudte løsningen. \n Herunder skal også funksjoner som er under utvikling eller under planlegging tilsvarende, og det skal angis planlagt ferdig-dato.'
+);
+
+INSERT INTO requirements (id, show_order, priority, functionality,
+                          requirement_text)
+VALUES (
+  2, 0, null, '1.4.1',
+  'Å ivareta kravene stilt i den generelle lovgivningen som berører offentlig forvaltning.'
+);
+
+INSERT INTO requirements (id, show_order, priority, functionality,
+                          requirement_text)
+VALUES (
+  3, 1, 'O', '1.4.1',
+  'Tilbudt løsning skal være godkjent jfr. NOARK5-standarden. Kopi av egenerklæringsskjema for NOARK5-godkjenning skal vedlegges.'
+);
+
+INSERT INTO requirements (id, show_order, priority, functionality,
+                          requirement_text)
+VALUES (
+  4, 2, 'O', '1.4.1',
+  'Tilbudt sak- og arkivløsning skal oppfylle alle obligatoriske krav til indre og ytre kjerne jf. NOARK5-standarden.'
+);
+
+INSERT INTO requirements (id, show_order, priority, functionality,
+                          requirement_text)
+VALUES (
+  5, 3, 'O', '1.4.1',
+  'Tilbudt sak- og arkivløsning skal oppfylle alle obligatoriske krav jf. NOARK5-standardens kapittel 7 - 13.'
+);
+
+
+INSERT INTO requirements (id, show_order, priority, functionality,
+                          requirement_text)
+VALUES (
+  6, 4, 'O', '1.4.1',
+  'Tilbudt sak- og arkivløsning skal oppfylle alle krav til indre og ytre kjerne merket B (Betinget obligatorisk) med unntak av de som spesifikt gjelder fagsystemer.'
+);
+
+
+INSERT INTO requirements (id, show_order, priority, functionality,
+                          requirement_text)
+VALUES (
+  7, 5, 'O', '1.4.1',
+  'Leverandøren skal ta ansvar for at uttrekk til arkivdepot fra levert løsning er korrekt og blir godkjent av oppdragsgivers arkivdepot.'
+);
+
+
+INSERT INTO requirements (id, show_order, priority, functionality,
+                          requirement_text)
+VALUES (
+  8, 6, '1', '1.4.1',
+  '«Track a case» - Spor saken: Det bør være mulig for innbyggerne å følge med på status for innmeldt sak. Eksempelvis ved bruk av eget sporingsnummer.'
+);
